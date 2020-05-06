4dedd18913d4a823887c54f502b81d79a9a5262c
taqm <warabii.taqm@gmail.com>
2020-05-06 21:10:09 +0900

    不要なファイル削除

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+
+import IndexPage from '../views/IndexPage.vue';
 
 Vue.use(VueRouter);
 
@@ -8,16 +9,9 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: IndexPage,
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+
 ];
 
 const router = new VueRouter({
